b015f5878a6c61beb3d7a860c13fa514c4ecc6b7
Christian Malpass <chriscellphonerepair@gmail.com>
2019-05-10 00:34:34 -0400

    Built prototype of notification bar for app. Will execute it next.

--- app/src/main/java/com/example/furofo/areyouconnected/MainActivity.java
+++ app/src/main/java/com/example/furofo/areyouconnected/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.furofo.areyouconnected;
 
+import android.app.NotificationChannel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("hello")
+                .setContentText("hello world")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
     }
 }
