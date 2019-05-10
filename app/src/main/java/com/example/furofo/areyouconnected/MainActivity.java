9e33d2f64e461974844a04dff6e5a140dab0edc2
Christian Malpass <chriscellphonerepair@gmail.com>
2019-05-10 15:14:11 -0400

    Made changes to notification bar to get to appear errors occured

--- app/src/main/java/com/example/furofo/areyouconnected/MainActivity.java
+++ app/src/main/java/com/example/furofo/areyouconnected/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.furofo.areyouconnected;
 
 import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +18,42 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+    /*    NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.notification_icon)
                 .setContentTitle("hello")
                 .setContentText("hello world")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this); */
+        private void createNotificationChannel() {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                CharSequence name = "hello";
+                String description = "description";
+                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+                channel.setDescription(description);
+                // Register the channel with the system; you can't change the importance
+                // or other notification behaviors after this
+                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
 
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("hello")
+                .setContentText("helloworld")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+      NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notificationId, builder.build());
 
     }
+
 }
