f0c7eb45308854280c9e97984d0510b19dc1ec51
Christian Malpass <chriscellphonerepair@gmail.com>
2019-05-11 22:42:38 -0400

    still working on errors issues creating and applying channel ID

--- app/src/main/java/com/example/furofo/areyouconnected/MainActivity.java
+++ app/src/main/java/com/example/furofo/areyouconnected/MainActivity.java
@@ -17,19 +17,19 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
     /*    NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.notification_icon)
                 .setContentTitle("hello")
                 .setContentText("hello world")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this); */
-        private void createNotificationChannel() {
+
             // Create the NotificationChannel, but only on API 26+ because
             // the NotificationChannel class is new and not in the support library
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 CharSequence name = "hello";
                 String description = "description";
+                String CHANNEL_ID = "Bob";
                 int importance = NotificationManager.IMPORTANCE_DEFAULT;
                 NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
                 channel.setDescription(description);
@@ -37,22 +37,28 @@ public class MainActivity extends AppCompatActivity {
                 // or other notification behaviors after this
                 NotificationManager notificationManager = getSystemService(NotificationManager.class);
                 notificationManager.createNotificationChannel(channel);
+
+                NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("hello")
+                        .setContentText("helloworld")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setChannelId("channel")
+                        .build();
             }
-        }
 
 
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.notification_icon)
-                .setContentTitle("hello")
-                .setContentText("helloworld")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+
 
 
       NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
 // notificationId is a unique int for each notification that you must define
-        notificationManager.notify(notificationId, builder.build());
+
+        notificationManager.notify(1, builder.build());
 
     }
 
