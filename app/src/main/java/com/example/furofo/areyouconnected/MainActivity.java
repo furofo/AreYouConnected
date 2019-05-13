29fd46ffd986f53a57401ff8c46c20dc63f09207
Christian Malpass <chriscellphonerepair@gmail.com>
2019-05-13 01:11:54 -0400

    further work on compile errors and linked button to notification, button currently crashes when we attempt to open

--- app/src/main/java/com/example/furofo/areyouconnected/MainActivity.java
+++ app/src/main/java/com/example/furofo/areyouconnected/MainActivity.java
@@ -2,64 +2,54 @@ package com.example.furofo.areyouconnected;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Build;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final String CHANNEL_ID = "wifiNotification";
+    private final int NOTIFICATION_ID = 001;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+    }
 
-    /*    NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.notification_icon)
-                .setContentTitle("hello")
-                .setContentText("hello world")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this); */
+        public void displayNotification(View view) {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+            builder.setContentTitle("Are you on Wifi");
+            builder.setContentText("this is simple notification");
+            builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-            // Create the NotificationChannel, but only on API 26+ because
-            // the NotificationChannel class is new and not in the support library
+            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+            notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+        }
+
+
+        private void createNotificationChannel() {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 CharSequence name = "hello";
-                String description = "description";
-                String CHANNEL_ID = "Bob";
+                String description = "Display if on wifi or not";
                 int importance = NotificationManager.IMPORTANCE_DEFAULT;
                 NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
                 channel.setDescription(description);
                 // Register the channel with the system; you can't change the importance
                 // or other notification behaviors after this
-                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 notificationManager.createNotificationChannel(channel);
 
-                NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.notification_icon)
-                        .setContentTitle("hello")
-                        .setContentText("helloworld")
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                        .setChannelId("channel")
-                        .build();
+
             }
+        }
 
-
-
-
-
-
-
-
-      NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-// notificationId is a unique int for each notification that you must define
-
-        notificationManager.notify(1, builder.build());
 
     }
 
-}
+
